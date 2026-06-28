ae47350f5b374c67bdd0e829df2a218d6d5b03aa
martinrpr <martinrpr26@gmail.com>
2026-06-28 10:15:57 -0400

    Add Stage2 project base

---
+++ Stage2/Stage2.cpp
@@ -0,0 +1,33 @@
+    #include <QApplication>
+    #include <QFileDialog>
+    #include <iostream>
+
+    #include "Territory.h"
+    #include "MainWindow.h"
+
+    int main(int argc, char *argv[])
+    {
+        QApplication a(argc, argv);
+
+        QString fileName = QFileDialog::getOpenFileName(
+            nullptr,
+            "Select Config File",
+            QDir::homePath(),
+            "Text Files (*.txt)"
+        );
+
+        if (fileName.isEmpty()){
+            return 0;
+        }
+
+        Territory territory;
+        if (!territory.loadConfiguration(fileName)) {
+            std::cerr << "Error al cargar el archivo de configuracion." << std::endl;
+            return -1;
+        }
+
+        MainWindow window(&territory);
+        window.show();
+
+        return QApplication::exec();
+    }
