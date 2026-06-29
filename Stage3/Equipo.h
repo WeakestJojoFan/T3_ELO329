741eff30fdf5cac869a77e38aa57cccce53c6c07
Nicolás Rogel <nicorogelking@gmail.com>
2026-06-29 14:43:41 -0400

    Add Equipo and EquipoPasivo classes for managing entity behavior and interactions

---
+++ Stage3/Equipo.h
@@ -0,0 +1,29 @@
+#ifndef EQUIPO_H
+#define EQUIPO_H
+#include <QDebug>
+#include <qobject.h>
+
+class Equipo : public QObject{
+    Q_OBJECT
+protected:
+    QString m_name;
+    QString m_itemName;
+    double m_x;
+    double m_y;
+    double m_speed;
+    double m_angle;
+    double m_deltaAngle;
+
+public:
+    Equipo(QString name, double x, double y, double speed, double angle, double deltaAngle);
+
+    QString getName() const;
+    QString getItemName() const;
+    double getX() const;
+    double getY() const;
+    void updatePosition(double deltaTime);
+    void reboteX();
+    void reboteY();
+};
+
+#endif // EQUIPO_H
---
+++ Stage3/EquipoPasivo.cpp
@@ -0,0 +1,27 @@
+#include "EquipoPasivo.h"
+#include <cmath>
+
+EquipoPasivo::EquipoPasivo(QString name, double x, double y, double speed, double angle, double deltaAngle): Equipo(name,x,y,speed,angle,deltaAngle){}
+
+void EquipoPasivo::findNearbyPhone(const QVector<Cellular*>& cells){
+    for (Cellular* cell : cells) {
+        double dist = std::sqrt( std::pow( (cell->getX()-m_x) ,2) + std::pow( (cell->getY() - m_y) , 2));
+        if (dist <= 50){
+            cell->reportItemPos(this);
+            break;
+        }
+    }
+}
+
+void EquipoPasivo::updatePosition(double deltaTime, const QVector<Cellular*>& cells){
+    Equipo::updatePosition(deltaTime);
+
+    m_timeSinceLastScan += deltaTime;
+
+    if (m_timeSinceLastScan >= m_bipTime){
+        findNearbyPhone(cells);
+        emit scanAnimation();
+
+        m_timeSinceLastScan -= m_bipTime;
+    }
+}
